42c031bc07e3456e729f5b0cbbac44e6d09f9067
JY Lee <52771148+xdrcftv@users.noreply.github.com>
2020-03-14 17:19:54 +0900

    Initial commit

---
+++ curvefit_practice.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# from scipy.optimize import curve_fit
+from scipy import integrate
+from text_parser import *
+from lmfit import Model
+import os
+
+CDBS_path = 'C:/Users/admin/Desktop/CDBS/'
+subdir_name = []
+subdir_path = []
+for root, dirs, files in os.walk(CDBS_path):
+    if len(dirs) > 0:
+        for dir_name in dirs:
+            subdir_name.append(dir_name)
+            subdir_path.append(CDBS_path+dir_name)
+    break
+
+CDBS_matrix = []
+for subdir in subdir_path:
+    CDBS_matrix.append(asc_parser(subdir))
+
+width = 10
+gmodel = Model(gaussian)
+
+normal_curves = []
+x_hr = np.linspace(-width, width, 10*width)
+
+for i, matrix in enumerate(CDBS_matrix):
+    x, y, max_point = find_sudo_peak(matrix, width=width)
+    params = gmodel.make_params(cen=max_point[0], amp=np.max(y)*(np.sqrt(2*np.pi)*width/2), wid= width/2)
+    result = gmodel.fit(y, params, x=x)
+    y_hr = gaussian(x_hr, 1, 0, wid=result.best_values['wid'])
+
+    # AUC = integrate.simps(result.best_fit, x)
+    # plt.plot(np.divide(result.best_fit, AUC), label=subdir_name[i])
+    # normal_curves.append(np.divide(result.best_fit, AUC))
+    plt.plot(y_hr, label=subdir_name[i])
+
+plt.legend(loc='best')
+plt.show()
+
+# plt.plot(x, y, 'bo')
+# plt.plot(x, result.init_fit, 'k--', label='initial fit')
+# plt.plot(x, result.best_fit, 'r-', label='best fit')
+# plt.legend(loc='best')
+# plt.show()
+# plt.plot(x, y, 'bo')
+# plt.plot(x, result.init_fit, 'k--', label='initial fit')
+# plt.plot(x, n_curve, 'r-', label='n_curve')
+# plt.legend(loc='best')
+# plt.show()
+
---
+++ text_parser.py
@@ -0,0 +1,52 @@
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks, peak_prominences
+from lmfit import Model
+import os
+from scipy import integrate
+
+
+def asc_parser(path):
+    asc_file_list = glob.glob(os.path.join(path, "*CDBS*.asc"))
+    f = open(asc_file_list[0], 'r')
+    lines = f.readlines()
+    CDBS_data = []
+    for i in range(1024):
+        count = lines[i+23].split(",")
+        count = list(map(int, count))
+        del count[0]
+        CDBS_data.append(count)
+
+    CDBS_data = np.array(CDBS_data, dtype=float)
+    return CDBS_data
+
+
+def find_subdir(root):
+    """ return subdir under root"""
+    subdir_path = []
+    for root, dirs, files in os.walk(root):
+        if len(dirs) > 0:
+            for dir_name in dirs:
+                subdir_path.append(root + dir_name)
+        break
+    return subdir_path
+
+
+def gaussian(x, amp, cen, wid):
+    """1-d gaussian: gaussian(x, amp, cen, wid)"""
+    return (amp / (np.sqrt(2 * np.pi) * wid)) * np.exp(-(x - cen) ** 2 / (2 * wid ** 2))
+
+
+gmodel = Model(gaussian)
+
+
+def find_sudo_peak(matrix, width):
+    """should be changed (peak detection algorithm needed)"""
+    """return chn_num(x), count(y), max_point(peak index)"""
+    max_point = np.squeeze(np.array(np.where(matrix == np.amax(matrix))))
+    chn_num = np.arange(max_point[1]-width, max_point[1]+width)
+    count = matrix[max_point[0], chn_num]
+    return chn_num, count, max_point
+
+
