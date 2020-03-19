cb0a1ecc18e0c126f7508dd9d706ca8c5b2c9b95
JY Lee <52771148+xdrcftv@users.noreply.github.com>
2020-03-19 15:58:29 +0900

    0319

--- Sample_Validation.py
+++ Sample_Validation.py
@@ -7,7 +7,13 @@ CDBS_files = glob.glob(os.path.join(dir_path, "*CDBS*.asc"))
 file_name =[]
 CDBS_matrix = {}
 
-for file in CDBS_files:
+width = 30
+gmodel = Model(gaussian)
+
+
+linestyle = ['o', '^', '>', 's','-.', ':', '--']
+
+for n, file in enumerate(CDBS_files):
     basename = os.path.basename(file)
     file_name.append(basename)
     f = open(file, 'r')
@@ -19,12 +25,26 @@ for file in CDBS_files:
         del count[0]
         CDBS_data.append(count)
     CDBS_matrix[basename] = ndimage.rotate(np.array(CDBS_data, dtype=float), -45, reshape=False)
+    x, y, max_point = find_sudo_peak(CDBS_matrix[basename], width=width)
 
-width = 10
-gmodel = Model(gaussian)
 
-x, y, max_point = find_sudo_peak(CDBS_matrix[file_name[0]], width=width)
-plt.bar(x, y)
+    # plt.plot(x, np.divide(y, AUC), '.', label=basename)
+
+    params = gmodel.make_params(cen=max_point[1], amp=np.max(y) * (np.sqrt(2 * np.pi) * width / 2), wid=width / 2)
+    result = gmodel.fit(y, params, x=x)
+    AUC = integrate.simps(result.best_fit, x)
+
+    x_adj = np.add(0.134*np.subtract(x, max_point[1]), 511)
+    print(result.best_values['wid'])
+    plt.plot(x_adj, np.divide(y, AUC), linestyle[n], label=basename)
+    # x_hr = np.linspace(x[0], x[-1], 10*len(x))
+    # y_hr = gaussian(x_hr, cen=result.best_values['cen'], amp=result.best_values['amp'], wid=result.best_values['wid'])
+    # plt.plot(x_hr, y_hr, '-')
+
+plt.yscale('log')
+plt.xlabel("keV")
+plt.ylabel("A.U.")
+plt.legend(loc='best')
 plt.show()
 # params = gmodel.make_params(cen=max_point[1], amp=np.max(y)*(np.sqrt(2*np.pi)*width/2), wid= width/2)
 # result = gmodel.fit(y, params, x=x)
