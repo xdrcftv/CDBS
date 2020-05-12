31f38c68eaf33032354ac34d0e33eb244c3a45b2
xdrcftv <xdrcftv@gmail.com>
2020-05-12 19:06:38 +0900

    0512

--- curvefit_practice.py
+++ curvefit_practice.py
@@ -1,7 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-# from scipy.optimize import curve_fit
-from scipy import integrate
 from text_parser import *
 from lmfit import Model
 import os
